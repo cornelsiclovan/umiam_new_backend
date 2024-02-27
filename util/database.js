818ca4dc997eca39d92745e8205c758fa635d383
cornel <cornel.siclovan@gmail.com>
2024-02-27 16:47:06 +0200

    change timezone

--- util/database.js
+++ util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("umiam_new", "root", "", {
   dialect: "mysql",
   host: "localhost",
+  timezone: "+02:00"
 });
 
 module.exports = sequelize;
