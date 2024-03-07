4a366c14d023fe8b6ad873c8cdea101c9babef14
cornel <cornel.siclovan@gmail.com>
2024-03-07 12:40:53 +0200

    totals per day orders

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,32 +6,31 @@ const Type = require("../models/type");
 const { deleteFile } = require("../util/file");
 const Order = require("../models/order");
 
-// get products per place 
+// get products per place
 exports.getProducts = async (req, res, next) => {
   const placeId = req.query.placeId;
   const categoryId = req.query.categoryId;
   const typeId = req.query.typeId;
 
-  let queryObject = {userId: req.userId};
+  let queryObject = { userId: req.userId };
 
-  if(placeId) {
+  if (placeId) {
     queryObject.placeId = placeId;
   }
 
-  if(typeId) {
+  if (typeId) {
     queryObject.typeId = typeId;
   }
 
-  if(categoryId) {
+  if (categoryId) {
     queryObject.categoryId = categoryId;
   }
 
   let products = [];
   let totalItems = 0;
 
-
   try {
-    products = await Product.findAll({'where': queryObject});
+    products = await Product.findAll({ where: queryObject });
 
     totalItems = products.length;
 
@@ -45,21 +44,20 @@ exports.getProducts = async (req, res, next) => {
           typeId: product.typeId,
           categoryId: product.categoryId,
           type: type.title,
-          category : category.title,
+          category: category.title,
           image: product.imageUrl,
           price: product.price,
-          place: product.placeId
-        }
+          place: product.placeId,
+        };
       })
-    )
-  
+    );
 
     res.status(200).json({
       products: producsToSend,
-      totalItems: totalItems
+      totalItems: totalItems,
     });
   } catch (error) {
-    next(error); 
+    next(error);
   }
 };
 
@@ -91,7 +89,7 @@ exports.createProduct = async (req, res, next) => {
   try {
     const place = await Place.findByPk(req.body.placeId);
 
-    if(place.userId !== req.userId) {
+    if (place.userId !== req.userId) {
       const error = new Error("Not authorized!");
       error.statusCode = 400;
       throw error;
@@ -99,30 +97,27 @@ exports.createProduct = async (req, res, next) => {
 
     const type = await Type.findByPk(req.body.typeId);
 
-    if(!type) {
+    if (!type) {
       const error = new Error("Invalid type");
       error.statusCode = 400;
       throw error;
     }
 
-
-    if(type.placeId !== place.id) {
+    if (type.placeId !== place.id) {
       const error = new Error("The type is not from the selected place");
       error.statusCode = 400;
       throw error;
-    } 
+    }
 
     const category = await Category.findByPk(req.body.categoryId);
 
-    
-    if(!category) {
+    if (!category) {
       const error = new Error("Invalid category");
       error.statusCode = 400;
       throw error;
     }
 
-
-    if(type.categoryId !== category.id) {
+    if (type.categoryId !== category.id) {
       const error = new Error("The type is not from the selected category");
       error.statusCode = 400;
       throw error;
@@ -134,10 +129,10 @@ exports.createProduct = async (req, res, next) => {
     //     throw error;
     // }
 
-    if(!req.files["image"]) {
+    if (!req.files["image"]) {
       const error = new Error("No file available");
       error.statusCode = 400;
-      throw error
+      throw error;
     }
 
     const title = req.body.title;
@@ -155,7 +150,7 @@ exports.createProduct = async (req, res, next) => {
       userId: req.userId,
       placeId: placeId,
       categoryId: categoryId,
-      typeId: typeId
+      typeId: typeId,
     });
 
     product.dataValues.type = type.title;
@@ -171,10 +166,9 @@ exports.createProduct = async (req, res, next) => {
 };
 
 exports.editProduct = async (req, res, next) => {
-  const productId = req.params.productId; 
+  const productId = req.params.productId;
   console.log(req.files["image"]);
   try {
-   
     const product = await Product.findByPk(productId);
 
     if (!product) {
@@ -189,27 +183,23 @@ exports.editProduct = async (req, res, next) => {
       throw error;
     }
 
-
     const type = await Type.findByPk(req.body.typeId);
 
-    if(!type) {
+    if (!type) {
       const error = new Error("Invalid type");
       error.statusCode = 400;
       throw error;
     }
 
-
     const category = await Category.findByPk(req.body.categoryId);
 
-    
-    if(!category) {
+    if (!category) {
       const error = new Error("Invalid category");
       error.statusCode = 400;
       throw error;
     }
 
-
-    if(type.categoryId !== category.id) {
+    if (type.categoryId !== category.id) {
       const error = new Error("The type is not from the selected category");
       error.statusCode = 400;
       throw error;
@@ -224,13 +214,11 @@ exports.editProduct = async (req, res, next) => {
 
     let image;
 
-    if(req.files["image"]) {
+    if (req.files["image"]) {
       image = req.files["image"][0].path;
     } else {
       image = product.imageUrl;
     }
-
-    
 
     product.title = updatedTitle;
     product.price = updatedPrice;
@@ -295,40 +283,38 @@ exports.getPlaces = async (req, res, next) => {
   let places = [];
   let totalItems = 0;
 
-
   try {
-    places = await Place.findAll({'where': {'userid': req.userId }});
+    places = await Place.findAll({ where: { userid: req.userId } });
 
     totalItems = places.length;
 
-    if(!places || places.length === 0) {
+    if (!places || places.length === 0) {
       const error = new Error("No places are found for this user!");
       throw error;
-    } 
+    }
 
     res.status(200).json({
       places: places,
-      totalItems: totalItems
+      totalItems: totalItems,
     });
   } catch (error) {
     next(error);
   }
-}
+};
 
 exports.getPlace = async (req, res, next) => {
   const placeId = req.params.placeId;
 
-  
   try {
     const place = await Place.findByPk(placeId);
-    
+
     if (!place) {
       const error = new Error("This place does not exist!");
       error.statusCode = 404;
       throw error;
     }
-    
-    if(place.userId !== req.userId) {
+
+    if (place.userId !== req.userId) {
       const error = new Error("Not authorized!");
       error.statusCode = 400;
       throw error;
@@ -343,14 +329,14 @@ exports.getPlace = async (req, res, next) => {
     }
     next(error);
   }
-}
+};
 
 exports.addPlace = async (req, res, next) => {
   const errors = validationResult(req);
 
   console.log(req.body);
 
-  if(!req.isOwner) {
+  if (!req.isOwner) {
     const error = new Error("Not authorized!");
     error.statusCode = 403;
     next(error);
@@ -363,7 +349,6 @@ exports.addPlace = async (req, res, next) => {
       error.data = errors.array();
       throw error;
     }
-
   } catch (error) {
     next(error);
     return;
@@ -392,19 +377,19 @@ exports.addPlace = async (req, res, next) => {
     }
     next(error);
   }
-}
+};
 
 exports.editPlace = async (req, res, next) => {
   const placeId = req.params.placeId;
-  
+
   console.log(req.isOwner);
 
-  if(!req.isOwner) {
+  if (!req.isOwner) {
     const error = new Error("Not authorized!");
     error.statusCode = 403;
     next(error);
   }
-  
+
   const errors = validationResult(req);
 
   try {
@@ -449,17 +434,17 @@ exports.editPlace = async (req, res, next) => {
     }
     next(error);
   }
-}
+};
 
 exports.deletePlace = async (req, res, next) => {
   const placeId = req.params.placeId;
-  
-  if(!req.isOwner) {
+
+  if (!req.isOwner) {
     const error = new Error("Not authorized!");
     error.statusCode = 403;
     next(error);
   }
-  
+
   try {
     const place = await Place.findByPk(placeId);
 
@@ -484,10 +469,9 @@ exports.deletePlace = async (req, res, next) => {
     }
     next(error);
   }
-}
+};
 
 //// END PLACE
-
 
 //// CATEGORY
 
@@ -495,12 +479,10 @@ exports.getCategories = async (req, res, next) => {
   let categories = [];
   let totalItems = 0;
 
-
   try {
-
     categories = await Category.findAll();
 
-    if(!categories || categories.length === 0) {
+    if (!categories || categories.length === 0) {
       const error = new Error("There is no category available");
       error.statusCode = 400;
       throw error;
@@ -508,41 +490,37 @@ exports.getCategories = async (req, res, next) => {
 
     res.status(200).json({
       categories: categories,
-      totalItems: totalItems
+      totalItems: totalItems,
     });
-
   } catch (error) {
     next(error);
   }
-
-}
+};
 
 exports.getCategory = async (req, res, next) => {
   const categoryId = req.params.categoryId;
 
   try {
-    
     const category = Category.findByPk(categoryId);
 
-    if(!category) {
+    if (!category) {
       const error = new Error("Category is not availble!");
       error.statusCode = 400;
       throw error;
     }
 
     res.status(200).json({
-      category: category
-    })
-
-  } catch(error) {
+      category: category,
+    });
+  } catch (error) {
     next(error);
   }
-}
+};
 
 exports.addCategory = async (req, res, next) => {
   const errors = validationResult(req);
 
-  if(!req.isAdmin) {
+  if (!req.isAdmin) {
     const error = new Error("Not authorized!");
     error.statusCode = 403;
     next(error);
@@ -555,7 +533,6 @@ exports.addCategory = async (req, res, next) => {
       error.data = errors.array();
       throw error;
     }
-
   } catch (error) {
     next(error);
     return;
@@ -563,29 +540,27 @@ exports.addCategory = async (req, res, next) => {
 
   const title = req.body.title;
   const description = req.body.description;
-  
-  try {
 
+  try {
     const category = await Category.create({
       title: title,
-      description: description
+      description: description,
     });
 
     res.status(200).json({
-      category: category
+      category: category,
     });
-
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
-}
+};
 
 exports.editCategory = async (req, res, next) => {
   const errors = validationResult(req);
 
   const categoryId = req.params.categoryId;
 
-  if(!req.isAdmin) {
+  if (!req.isAdmin) {
     const error = new Error("Not authorized!");
     error.statusCode = 403;
     next(error);
@@ -598,7 +573,6 @@ exports.editCategory = async (req, res, next) => {
       error.data = errors.array();
       throw error;
     }
-
   } catch (error) {
     next(error);
     return;
@@ -608,46 +582,40 @@ exports.editCategory = async (req, res, next) => {
   const description = req.body.description;
 
   try {
-
     const category = await Category.findByPk(categoryId);
 
-
-
-    if(!category) {
+    if (!category) {
       const error = new Error("Category does not exist!");
       error.statusCode = 400;
       throw error;
     }
 
-
     category.title = title;
     category.description = description;
-    
+
     await category.save();
 
     res.status(200).json({
-      category: category
-    })
-
-  } catch(error) {
+      category: category,
+    });
+  } catch (error) {
     next(error);
   }
-}
+};
 
 exports.deleteCategory = async (req, res, next) => {
   const categoryId = req.params.categoryId;
 
-  if(!req.isAdmin) {
+  if (!req.isAdmin) {
     const error = new Error("Not authorized!");
     error.statusCode = 403;
     next(error);
   }
 
-
-  try{
+  try {
     const category = await Category.findByPk(categoryId);
-    
-    if(!category) {
+
+    if (!category) {
       const error = new Error("This category does not exist!");
       error.statusCode = 400;
       throw error;
@@ -656,13 +624,12 @@ exports.deleteCategory = async (req, res, next) => {
     await category.destroy();
 
     res.status(200).json({
-      message: "Category deleted!"
+      message: "Category deleted!",
     });
-
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
-}
+};
 
 //// END CATEGORY
 
@@ -674,33 +641,32 @@ exports.getTypes = async (req, res, next) => {
   const placeId = req.params.placeId;
   const categoryId = req.body.categoryId;
 
-  
+  let queryObject = { placeId: placeId };
 
-  let queryObject = {placeId: placeId};
-
-  if(categoryId) {
+  if (categoryId) {
     queryObject.categoryId = categoryId;
   }
 
-  if(!placeId) {
+  if (!placeId) {
     const error = new Error("please provide place id");
     error.statusCode = 403;
     next(error);
   }
 
   try {
-    types = await Type.findAll({'where': queryObject});
+    types = await Type.findAll({ where: queryObject });
 
-    if(!types || types.length === 0) {
+    if (!types || types.length === 0) {
       const error = new Error("No types are available");
       error.statusCode = 400;
       throw error;
     }
 
-
     let typesToSend = await Promise.all(
-      types.map(async(type) => {
-        let category = await Category.findAll({'where': {id: type.categoryId}});
+      types.map(async (type) => {
+        let category = await Category.findAll({
+          where: { id: type.categoryId },
+        });
         console.log(category[0].title);
 
         return {
@@ -711,46 +677,43 @@ exports.getTypes = async (req, res, next) => {
           description: type.description,
           createdAt: type.createdAt,
           updatedAt: type.updatedAt,
-        }
+        };
       })
     );
-    
 
     res.status(200).json({
       types: typesToSend,
-      totalItems: types.length
-    })
-
-  } catch(error) {
+      totalItems: types.length,
+    });
+  } catch (error) {
     next(error);
   }
-}
+};
 
 exports.getType = async (req, res, next) => {
-  let  typeId = req.params.typeId;
+  let typeId = req.params.typeId;
 
   try {
     const type = Type.findByPk(typeId);
 
-    if(!type) {
+    if (!type) {
       const error = "This type does not exist";
       error.statusCode = 400;
       throw error;
     }
 
-
     res.status(200).json({
-      type: type
+      type: type,
     });
   } catch (error) {
     next(error);
   }
-}
+};
 
 exports.addType = async (req, res, next) => {
   const errors = validationResult(req);
 
-  if(!req.isOwner) {
+  if (!req.isOwner) {
     const error = new Error("Not authorized!");
     error.statusCode = 403;
     next(error);
@@ -763,7 +726,6 @@ exports.addType = async (req, res, next) => {
       error.data = errors.array();
       throw error;
     }
-
   } catch (error) {
     next(error);
     return;
@@ -773,15 +735,11 @@ exports.addType = async (req, res, next) => {
   const description = req.body.description;
   const placeId = req.body.placeId;
   const categoryId = req.body.categoryId;
-  
-
-
 
   try {
-
     const category = await Category.findByPk(categoryId);
 
-    if(!category) {
+    if (!category) {
       const error = new Error("Category does not exist");
       error.statusCode = 400;
       throw error;
@@ -789,65 +747,56 @@ exports.addType = async (req, res, next) => {
 
     const place = await Place.findByPk(placeId);
 
-    if(!place) {
+    if (!place) {
       const error = new Error("Place does not exist!");
       error.statusCode = 400;
       throw error;
     }
 
-    if(place.userId !== req.userId) {
-  
+    if (place.userId !== req.userId) {
       const error = new Error("Not owner authorized!");
       error.statusCode = 400;
       throw error;
     }
-
 
     const type = await Type.create({
       title: title,
       description: description,
       userId: req.userId,
       placeId: placeId,
-      categoryId: categoryId
+      categoryId: categoryId,
     });
 
-    
-   
     type.dataValues.category = category.title;
 
-
     res.status(200).json({
-      type: type
+      type: type,
     });
-
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
-}
+};
 
 exports.editType = async (req, res, next) => {
   const errors = validationResult(req);
- 
-  try {
 
-    if(!req.isOwner) {
+  try {
+    if (!req.isOwner) {
       const error = new Error("Not authorized!");
       error.statusCode = 403;
       throw error;
     }
-  
+
     if (!errors.isEmpty()) {
       const error = new Error("Validation failed, entered data is incorrect.");
       error.status = 422;
       error.data = errors.array();
       throw error;
     }
-
   } catch (error) {
     next(error);
     return;
   }
-
 
   const title = req.body.title;
   const description = req.body.description;
@@ -855,13 +804,10 @@ exports.editType = async (req, res, next) => {
   const categoryId = req.body.categoryId;
   const typeId = req.params.typeId;
 
-
-
   try {
-
     const type = await Type.findByPk(typeId);
 
-    if(!type) {
+    if (!type) {
       const error = new Error("Type is not available");
       error.statusCode = 400;
       throw error;
@@ -869,7 +815,7 @@ exports.editType = async (req, res, next) => {
 
     const category = await Category.findByPk(categoryId);
 
-    if(!category) {
+    if (!category) {
       const error = new Error("Category does not exist");
       error.statusCode = 400;
       throw error;
@@ -877,29 +823,26 @@ exports.editType = async (req, res, next) => {
 
     const place = await Place.findByPk(placeId);
 
-    if(!place) {
+    if (!place) {
       const error = new Error("Place does not exist!");
       error.statusCode = 400;
       throw error;
     }
 
-    if(place.userId !== req.userId) {
-  
+    if (place.userId !== req.userId) {
       const error = new Error("Not authorized!");
       error.statusCode = 400;
       throw error;
     }
 
     type.title = title;
-    type.description = description;  
+    type.description = description;
     type.categoryId = categoryId;
     type.placeId = placeId;
 
     await type.save();
-    
+
     type.dataValues.category = category.title;
-
-
 
     // const type = await Type.create({
     //   title: title,
@@ -910,28 +853,26 @@ exports.editType = async (req, res, next) => {
     // });
 
     res.status(200).json({
-      type: type
+      type: type,
     });
-
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
-}
+};
 
 exports.deleteType = async (req, res, next) => {
   const typeId = req.params.typeId;
 
   try {
-
     const type = await Type.findByPk(typeId);
 
-    if(!type) {
+    if (!type) {
       const error = new Error("This type is not available");
       error.statusCode = 400;
       throw error;
     }
 
-    if(type.userId !== req.userId) {
+    if (type.userId !== req.userId) {
       const error = new Error("Not authorized");
       error.statusCode = 400;
       throw error;
@@ -940,27 +881,47 @@ exports.deleteType = async (req, res, next) => {
     await type.destroy();
 
     res.status(200).json({
-      message: "Type deleted"
-    })
-
-  } catch(error) {  
+      message: "Type deleted",
+    });
+  } catch (error) {
     next(error);
   }
-}
+};
 
 //// END TYPE
 
 exports.getOrders = async (req, res, next) => {
   let orders = [];
-  
-  try {
-     orders = await Order.findAll({limit: 200, order: [['id', 'DESC']]});
+  let ordersTotal = [];
 
-     res.status(200).json({
+  try {
+    orders = await Order.findAll({ limit: 200, order: [["id", "DESC"]] });
+
+    let total = 0;
+
+    for (let i = 0; i < orders.length; i++) {
+      if (i === 0) {
+        total = orders[i].total;
+      }
+
+      if (i > 0) {
+        if (
+          orders[i].createdAt.getDay() === orders[i - 1].createdAt.getDay() &&
+          orders[i].createdAt.getMonth() === orders[i - 1].createdAt.getMonth()
+        ) {
+          total = total + orders[i].total;
+        } else {
+          ordersTotal.push({ data: orders[i - 1].createdAt, total: total });
+          total = orders[i].total;
+        }
+      }
+    }
+    res.status(200).json({
       totalItems: orders.length,
-      orders: orders
-     })
+      orders: orders,
+      ordersTotal: ordersTotal,
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
